666fd877a175ef80953496b29d4baafd49a4a18e
York Wang <yw544@nyu.edu>
2018-01-04 15:13:47 +0000

    automatically backed up by learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -63,10 +63,10 @@ end
 def runner
   # code runner here
   welcome
-  initial_round = card_total
+  card_total = initial_round
   until card_total >21
     card_total = hit?(card_total)
-    display_card_total
+    display_card_total(card_total)
   end
 end_game(card_total)
 
