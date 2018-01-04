c37294dbed686d0b64aa52cd98c28ccec3a3ef94
York Wang <yw544@nyu.edu>
2018-01-04 15:10:27 +0000

    automatically backed up by learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -16,12 +16,13 @@ end
 
 def prompt_user
   # code #prompt_user here
-  puts "Type 'h' to thi or 's' to stay"
+  puts "Type 'h' to hit or 's' to stay"
 end
 
 def get_user_input
   # code #get_user_input here
-  puts "Type 'h' to hit or 's' to stay"
+gets.chomp
+
 end
 
 def end_game (over)
