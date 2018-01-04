955f3b27b1520221c7a28c08f4c922e4062762ae
York Wang <yw544@nyu.edu>
2018-01-04 15:13:00 +0000

    automatically backed up by learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -62,4 +62,12 @@ end
 
 def runner
   # code runner here
+  welcome
+  initial_round = card_total
+  until card_total >21
+    card_total = hit?(card_total)
+    display_card_total
+  end
+end_game(card_total)
+   
 end
